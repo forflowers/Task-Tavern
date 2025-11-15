ff329440481c53ecfa03b30ec04bc9896209c313
anema006 <anem@ALenovo.localdomain>
2025-11-15 10:30:04 -0800

    i think i got the base code in

--- desktop-buddy/main.py
+++ desktop-buddy/main.py
@@ -1,3 +1,11 @@
 from flask  import Flask, render_template
 
 app = Flask('desktop_buddy')
+
+
+@app.route('/')
+def home():
+	return "<h1>Hello!"
+
+if __name__ == '__main__':
+	app.run(debug=True)
